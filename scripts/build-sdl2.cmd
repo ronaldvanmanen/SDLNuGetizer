2c7ca65516c63fc81774e132520f46943f10ad12
Ronald van Manen <rvanmanen@gmail.com>
2023-06-15 15:55:44 +0200

    Use vcvarsall.bat to set environment variables to configure the command line for native 32-bit or 64-bit compilation.

--- scripts/build-sdl2.cmd
+++ scripts/build-sdl2.cmd
@@ -1,3 +1,30 @@
 @ECHO OFF
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build-sdl2.ps1""" %*"
+
+IF "%1"=="-architecture" (
+    SET architecture=%2
+) ELSE (
+    ECHO.
+    ECHO NAME
+    ECHO     %~dpnx0
+    ECHO.
+    ECHO SYNOPSIS
+    ECHO     Builds Windows native NuGet package for SDL2.
+    ECHO.
+    ECHO.
+    ECHO SYNTAX
+    ECHO     %~dpnx0 -architecture ^<String^>
+    ECHO.
+    ECHO.
+    ECHO DESCRIPTION
+    ECHO     Builds Windows native NuGet package for SDL2.
+    ECHO.
+    ECHO.
+    ECHO PARAMETERS
+    ECHO     -architecture ^<String^>
+    ECHO.
+    EXIT /B -1
+)
+
+call "%~dp0vcvarsall.cmd" %architecture%
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "&."%~dp0build-sdl2.ps1""" %*"
 EXIT /B %ERRORLEVEL%
