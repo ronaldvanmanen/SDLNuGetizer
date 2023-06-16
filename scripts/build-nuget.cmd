5f991f0862a58b0f7e1d91a0fb87d129d28c27d6
Ronald van Manen <rvanmanen@gmail.com>
2023-06-16 11:04:20 +0200

    Use pwsh.exe instead of powershell.exe.
    The binary name for PowerShell has been changed from powershell(.exe) to pwsh(.exe) . This change provides a deterministic way for users to run PowerShell on machines and support side-by-side installations of Windows PowerShell and PowerShell. See https://learn.microsoft.com/en-us/powershell/scripting/whats-new/differences-from-windows-powershell?view=powershell-7.3.

--- scripts/build-nuget.cmd
+++ scripts/build-nuget.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build-nuget.ps1""" %*"
+pwsh.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build-nuget.ps1""" %*"
 EXIT /B %ERRORLEVEL%
--- scripts/update-sdl2.cmd
+++ scripts/update-sdl2.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0update-sdl2.ps1""" %*"
+pwsh.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0update-sdl2.ps1""" %*"
 EXIT /B %ERRORLEVEL%
