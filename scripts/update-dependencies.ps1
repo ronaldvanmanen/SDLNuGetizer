696f17a290e0f1d6c70c37efa4b2b2df18e6488e
Ronald van Manen <rvanmanen@gmail.com>
2023-06-27 14:29:01 +0200

    Update 'update-dependencies,ps1' script.

--- scripts/update-dependencies.ps1
+++ scripts/update-dependencies.ps1
@@ -1,78 +1,110 @@
-function New-TemporaryDirectory {
-    $parent = [System.IO.Path]::GetTempPath()
-    [string] $name = [System.Guid]::NewGuid()
-    New-Item -ItemType Directory -Path (Join-Path $parent $name)
-}
-
 function Get-GitHubLatestRelease([string] $Owner, [string] $Repo, [string] $Bearer) {
-    $Uri = "https://api.github.com/repos/$Owner/$Repo/releases/latest"
-    $Headers = @{
-        'Accept'='application/vnd.github+json'
-        'Authorization'="Bearer $Bearer"
-        'X-GitHub-Api-Version'='2022-11-28'
-    }
-    Invoke-RestMethod -Headers $Headers -Uri $Uri
+  $Uri = "https://api.github.com/repos/$Owner/$Repo/releases/latest"
+  $Headers = @{
+    'Accept'='application/vnd.github+json'
+    'Authorization'="Bearer $Bearer"
+    'X-GitHub-Api-Version'='2022-11-28'
+  }
+  Invoke-RestMethod -Headers $Headers -Uri $Uri
 }
 
 function New-GitHubPullRequest([string] $Owner, [string] $Repo, [string] $Title, [string] $Head, [string] $Base, [string] $Bearer) {
-    $Body = "{""title"":""$Title"",""head"":""$Head"",""base"":""$Base""}"
-    $Headers = @{
-        'Accept'='application/vnd.github+json'
-        'Authorization'="Bearer $Bearer"
-        'X-GitHub-Api-Version'='2022-11-28'
-    }
-    $Uri = "https://api.github.com/repos/$Owner/$Repo/pulls"
-    Invoke-RestMethod -Body $Body -Headers $Headers -Method Post -Uri $Uri
+  $Body = "{""title"":""$Title"",""head"":""$Head"",""base"":""$Base""}"
+  $Headers = @{
+    'Accept'='application/vnd.github+json'
+    'Authorization'="Bearer $Bearer"
+    'X-GitHub-Api-Version'='2022-11-28'
+  }
+  $Uri = "https://api.github.com/repos/$Owner/$Repo/pulls"
+  Invoke-RestMethod -Body $Body -Headers $Headers -Method Post -Uri $Uri
 }
 
-$LibraryName = 'SDL'
-$LibraryRepoOwner = 'libsdl-org'
-$LibraryRepo = 'SDL'
+try {
+  $ScriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
 
-$PackageName = 'SDL2'
-$PackagingRepoOwner = 'ronaldvanmanen'
-$PackagingRepo = 'SDL2-packaging'
+  $RepoRoot = Join-Path -Path $PSScriptRoot -ChildPath ".."
+  
+  $LibraryName = 'SDL'
+  $LibraryRepoOwner = 'libsdl-org'
+  $LibraryRepo = 'SDL'
 
-$RepoRoot = Join-Path -Path $PSScriptRoot -ChildPath ".."
+  $PackageName = 'SDL2'
+  $PackagingRepoOwner = 'ronaldvanmanen'
+  $PackagingRepo = 'SDL2-packaging'
 
-$WorkTreeRoot = Join-Path -Path $RepoRoot -ChildPath ".."
+  Write-Host "${ScriptName}: Getting latest release of library $LibraryName...'" -ForegroundColor Yellow
+  $LatestLibraryRelease = Get-GitHubLatestRelease -Owner $LibraryRepoOwner -Repo $LibraryRepo -Bearer $Env:GITHUB_TOKEN
+  $LatestLibraryVersion = $LatestLibraryRelease.name
+  Write-Host "${ScriptName}: Latest release of library $LibraryName is '$LatestLibraryVersion'" -ForegroundColor Yellow
 
-$LatestLibraryRelease = Get-GitHubLatestRelease -Owner $LibraryRepoOwner -Repo $LibraryRepo -Bearer $Env:GITHUB_TOKEN
-$LatestLibraryVersion = $LatestLibraryRelease.name
-Write-Host "Latest release of library $LibraryName is '$LatestLibraryVersion'" -ForegroundColor Yellow
+  Write-Host "${ScriptName}: Getting latest release of package $PackageName...'" -ForegroundColor Yellow
+  $LatestPackageRelease = Get-GitHubLatestRelease -Owner $PackagingRepoOwner -Repo $PackagingRepo -Bearer $Env:GITHUB_TOKEN
+  $LatestPackageVersion = $LatestPackageRelease.name
+  Write-Host "${ScriptName}: Latest release of package $PackageName is '$LatestPackageVersion'" -ForegroundColor Yellow
 
-$LatestPackageRelease = Get-GitHubLatestRelease -Owner $PackagingRepoOwner -Repo $PackagingRepo -Bearer $Env:GITHUB_TOKEN
-$LatestPackageVersion = $LatestPackageRelease.name
-Write-Host "Latest release of package $PackageName is '$LatestPackageVersion'" -ForegroundColor Yellow
+  if ($LatestPackageVersion -eq $LatestLibraryVersion) {
+      Write-Host "${ScriptName}: We're up-to-date... nothing to do" -ForegroundColor Yellow
+      Break
+  }
 
-if ($LatestPackageVersion -eq $LatestLibraryVersion) {
-    Write-Host "We're up-to-date... nothing to do" -ForegroundColor Yellow
-    Break
+  $GitBranchName = "release/$LatestLibraryVersion-test"
+  Write-Host "${ScriptName}: Checking out branch $GitVersionConfigPath...'" -ForegroundColor Yellow
+  & git checkout -b $GitBranchName main
+  if ($LastExitCode -ne 0) {
+    throw "${ScriptName}: Failed to checkout branch $GitBranchName."
+  }
+
+  $GitVersionConfigPath = "$RepoRoot\GitVersion.yml"
+
+  Write-Host "${ScriptName}: Updating $GitVersionConfigPath...'" -ForegroundColor Yellow
+  $GitVersionConfig = Get-Content $GitVersionConfigPath
+  $UpdatedGitVersionConfig = $GitVersionConfig -Replace 'next-version: ([^\r\n]+)', "next-version: $LatestLibraryVersion"
+  Set-Content $GitVersionConfigPath $UpdatedGitVersionConfig -NoNewline
+
+  Push-Location 'sources\SDL'
+
+  Write-Host "${ScriptName}: Fetching changes in submodule 'sources/SDL'...'" -ForegroundColor Yellow
+  & git fetch
+  if ($LastExitCode -ne 0) {
+    throw "${ScriptName}: Failed to fetch changes in submodule 'sources/SDL'."
+  }
+
+  $LibraryReleaseTag = "release-$LatestLibraryVersion"
+  
+  Write-Host "${ScriptName}: Checking out tag '$LibraryReleaseTag' in submodule 'sources/SDL'...'" -ForegroundColor Yellow
+  & git checkout "$LibraryReleaseTag"
+  if ($LastExitCode -ne 0) {
+    throw "${ScriptName}: Failed to checkout tag '$LibraryReleaseTag' in submodule 'sources/SDL'."
+  }
+
+  Pop-Location
+
+  Write-Host "${ScriptName}: Staging changes to $GitVersionConfigPath...'" -ForegroundColor Yellow
+  & git add .
+  if ($LastExitCode -ne 0) {
+    throw "${ScriptName}: Failed add to $GitVersionConfigPath to commit."
+  }
+
+  $GitCommitMessage = "Bump $PackageName from $LatestPackageVersion to $LatestLibraryVersion"
+  Write-Host "${ScriptName}: Creating new commit '$GitCommitMessage'...'" -ForegroundColor Yellow
+  & git commit -m "$GitCommitMessage"
+  if ($LastExitCode -ne 0) {
+    throw "${ScriptName}: Failed to create new commit."
+  }
+
+  Write-Host "${ScriptName}: Pushing commit(s) on $GitBranchName to 'origin'..." -ForegroundColor Yellow
+  # & git push --set-upstream origin $GitBranchName
+  # if ($LastExitCode -ne 0) {
+  #   throw "${ScriptName}: Failed push commit(s) on $GitBranchName to 'origin'."
+  # }
+
+  $PullRequestTitle = "Bump $PackageName from $LatestPackageVersion to $LatestLibraryVersion"
+  Write-Host "${ScriptName}: Creating pull request '$PullRequestTitle' on GitHub..." -ForegroundColor Yellow
+  # New-GitHubPullRequest -Owner $PackagingRepoOwner -Repo $PackagingRepo -Title $PullRequestTitle -Head $GitBranchName -Base 'main' -Bearer $Env:GITHUB_TOKEN
 }
-
-$WorkTreePath = "$WorkTreeRoot\$PackagingRepo-bump_to_$LatestLibraryVersion"
-
-& git worktree add $WorkTreePath main
-
-Push-Location $WorkTreePath
-
-$GitVersionConfigPath = "$WorkTreePath\GitVersion.yml"
-$GitVersionConfig = Get-Content $GitVersionConfigPath
-$UpdatedGitVersionConfig = $GitVersionConfig -Replace 'next-version: ([^\r\n]+)', "next-version: $LatestLibraryVersion"
-Set-Content $GitVersionPath $UpdatedGitVersionConfig -NoNewline
-
-$GitBranchName = "release/$LatestLibraryVersion"
-& git checkout -b $GitBranchName
-& git add $GitVersionPath
-& git commit -m "Bump $PackageName from $LatestPackageVersion to $LatestLibraryVersion"
-& git push --set-upstream origin $GitBranchName
-
-$PullRequestTitle = "Bump $PackageName from $LatestPackageVersion to $LatestLibraryVersion"
-
-New-GitHubPullRequest -Owner $PackagingRepoOwner -Repo $PackagingRepo -Title $PullRequestTitle -Head $GitBranchName -Base 'main' -Bearer $Env:GITHUB_TOKEN
-
-Write-Host $GitHubPullRequestResponse
-
-Pop-Location
-
-& git worktree remove $WorkTreePath
+catch {
+  Write-Host -Object $_
+  Write-Host -Object $_.Exception
+  Write-Host -Object $_.ScriptStackTrace
+  exit 1
+}
