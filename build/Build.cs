811149c2e3f2dc316e3c3d53df6321affa2c6545
Ronald van Manen <rvanmanen@gmail.com>
2024-01-07 20:13:14 +0100

    Setup Nuke build.

---
+++ build/Build.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static System.Runtime.InteropServices.RuntimeInformation;
+
+class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+        });
+
+}
