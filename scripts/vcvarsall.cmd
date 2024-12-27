ea3c6db52aae126374f52d791b2a9ed025f3e94e
Ronald van Manen <rvanmanen@gmail.com>
2024-12-27 10:22:07 +0100

    Remove old build scripts.

--- scripts/vcvarsall.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-for /f "usebackq tokens=*" %%i in (`%~dp0vswhere.cmd -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
-  set InstallDir=%%i
-)
-
-if exist "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" (
-  call "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" %*
-)
